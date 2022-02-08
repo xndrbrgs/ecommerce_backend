69fdb718854e2ba48a6cab62e03c14ee9bc72992
xndrbrgs <aborgescolon@gmail.com>
2022-02-07 19:43:54 -0500

    need to fix products

--- models/index.js
+++ models/index.js
@@ -11,12 +11,11 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "SET NULL"
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "tag_id"
+  foreignKey: "product_id"
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
